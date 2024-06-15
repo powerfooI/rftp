16f306ffb059ed13bbf9fe1f605a269252b56a0c
Wangshuyi <whiteffire@163.com>
2024-06-15 12:46:30 +0800

    chore(rename): rename messages to commands

--- src/server/commands.rs
+++ src/server/commands.rs
@@ -1,41 +1,42 @@
 use std::net::SocketAddr;
 
 /**
-  USER <SP> <username> <CRLF>
-  PASS <SP> <password> <CRLF>
-  ACCT <SP> <account-information> <CRLF>
-  CWD  <SP> <pathname> <CRLF>
-  CDUP <CRLF>
-  SMNT <SP> <pathname> <CRLF>
-  QUIT <CRLF>
-  REIN <CRLF>
-  PORT <SP> <host-port> <CRLF>
-  PASV <CRLF>
-  TYPE <SP> <type-code> <CRLF>
-  STRU <SP> <structure-code> <CRLF>
-  MODE <SP> <mode-code> <CRLF>
-  RETR <SP> <pathname> <CRLF>
-  STOR <SP> <pathname> <CRLF>
-  STOU <CRLF>
-  APPE <SP> <pathname> <CRLF>
-  ALLO <SP> <decimal-integer>
-      [<SP> R <SP> <decimal-integer>] <CRLF>
-  REST <SP> <marker> <CRLF>
-  RNFR <SP> <pathname> <CRLF>
-  RNTO <SP> <pathname> <CRLF>
-  ABOR <CRLF>
-  DELE <SP> <pathname> <CRLF>
-  RMD  <SP> <pathname> <CRLF>
-  MKD  <SP> <pathname> <CRLF>
-  PWD  <CRLF>
-  LIST [<SP> <pathname>] <CRLF>
-  NLST [<SP> <pathname>] <CRLF>
-  SITE <SP> <string> <CRLF>
-  SYST <CRLF>
-  STAT [<SP> <pathname>] <CRLF>
-  HELP [<SP> <string>] <CRLF>
-  NOOP <CRLF>
- */
+ USER <SP> <username> <CRLF>
+ PASS <SP> <password> <CRLF>
+ ACCT <SP> <account-information> <CRLF>
+ CWD  <SP> <pathname> <CRLF>
+ CDUP <CRLF>
+ SMNT <SP> <pathname> <CRLF>
+ QUIT <CRLF>
+ REIN <CRLF>
+ PORT <SP> <host-port> <CRLF>
+ PASV <CRLF>
+ TYPE <SP> <type-code> <CRLF>
+ STRU <SP> <structure-code> <CRLF>
+ MODE <SP> <mode-code> <CRLF>
+ RETR <SP> <pathname> <CRLF>
+ STOR <SP> <pathname> <CRLF>
+ STOU <CRLF>
+ APPE <SP> <pathname> <CRLF>
+ ALLO <SP> <decimal-integer>
+     [<SP> R <SP> <decimal-integer>] <CRLF>
+ REST <SP> <marker> <CRLF>
+ RNFR <SP> <pathname> <CRLF>
+ RNTO <SP> <pathname> <CRLF>
+ ABOR <CRLF>
+ DELE <SP> <pathname> <CRLF>
+ RMD  <SP> <pathname> <CRLF>
+ MKD  <SP> <pathname> <CRLF>
+ PWD  <CRLF>
+ LIST [<SP> <pathname>] <CRLF>
+ NLST [<SP> <pathname>] <CRLF>
+ SITE <SP> <string> <CRLF>
+ SYST <CRLF>
+ STAT [<SP> <pathname>] <CRLF>
+ HELP [<SP> <string>] <CRLF>
+ NOOP <CRLF>
+ CDUP <CRLF>
+*/
 
 #[derive(Debug, Clone)]
 pub enum FtpCommand {
@@ -49,7 +50,7 @@ pub enum FtpCommand {
   ABOR,
   QUIT,
   SYST,
-  TYPE,
+  TYPE(String),
   RNFR(String),
   RNTO(String),
   PWD,
@@ -67,6 +68,7 @@ pub enum FtpCommand {
   ALLO(u64),
   NOOP,
   FEAT,
+  CDUP,
 }
 
 pub fn parse_command(req: String) -> FtpCommand {
@@ -79,22 +81,25 @@ pub fn parse_command(req: String) -> FtpCommand {
     "PASS" => FtpCommand::PASS(arg.to_string()),
     "PORT" => {
       let mut iter = arg.split(',').map(|s| s.parse::<u8>().unwrap());
-      let ip = iter.by_ref().take(4).map(|i| i.to_string()).collect::<Vec<String>>().join(".");
+      let ip = iter
+        .by_ref()
+        .take(4)
+        .map(|i| i.to_string())
+        .collect::<Vec<String>>()
+        .join(".");
       let p1 = iter.next().unwrap() as u16;
       let p2 = iter.next().unwrap() as u16;
       let port = p1 * 256 + p2; // FTP uses two bytes for the port number
       let addr = format!("{}:{}", ip, port);
       FtpCommand::PORT(addr.parse().unwrap())
     }
-    "PASV" => {
-      FtpCommand::PASV
-    }
+    "PASV" => FtpCommand::PASV,
     "RETR" => FtpCommand::RETR(arg.to_string()),
     "STOR" => FtpCommand::STOR(arg.to_string()),
     "ABOR" => FtpCommand::ABOR,
     "QUIT" => FtpCommand::QUIT,
     "SYST" => FtpCommand::SYST,
-    "TYPE" => FtpCommand::TYPE,
+    "TYPE" => FtpCommand::TYPE(arg.to_string()),
     "RNFR" => FtpCommand::RNFR(arg.to_string()),
     "RNTO" => FtpCommand::RNTO(arg.to_string()),
     "PWD" => FtpCommand::PWD,
@@ -107,7 +112,7 @@ pub fn parse_command(req: String) -> FtpCommand {
       } else {
         FtpCommand::LIST(Some(arg.to_string()))
       }
-    },
+    }
     "REST" => FtpCommand::REST,
     "DELE" => FtpCommand::DELE(arg.to_string()),
     "STAT" => FtpCommand::STAT,
@@ -115,9 +120,10 @@ pub fn parse_command(req: String) -> FtpCommand {
     "APPE" => FtpCommand::APPE(arg.to_string()),
     "ALLO" => FtpCommand::ALLO(arg.parse().unwrap()),
     "FEAT" => FtpCommand::FEAT,
+    "CDUP" => FtpCommand::CDUP,
     _ => {
       println!("Unknown command: {}, Args: {}", cmd, arg);
       FtpCommand::NOOP
-    },
+    }
   }
 }
